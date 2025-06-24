ef402f1176a56ffc9a0e0a4e8bd4def4e1821c79
Rajesh <rajeshlenka19999@gmail.com>
2025-06-24 11:22:54 +0530

    feat added vercel analytics and speed insgts

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Provider } from 'react-redux';
 import { store } from './store';
+
+// Enable Vercel Analytics
 import { inject } from '@vercel/analytics';
 inject(); 
 
-import { inject as injectSpeed } from '@vercel/speed-insights'
-injectSpeed()
-
+// Enable Vercel Speed Insights
+import { inject as injectSpeed } from '@vercel/speed-insights';
+injectSpeed();
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
